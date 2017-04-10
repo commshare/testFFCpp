16fe29b8087da944305d9143056e4c6bb9ceaa38
zhangbin <zhangbincodec@gmail.com>
2017-04-10 08:55:12 -0700

    tab to 4 space http://blog.csdn.net/wfdtxz/article/details/8520629

---
+++ av_common.h
@@ -0,0 +1,30 @@
+#ifndef _AV_COMMON_H_
+#define _AV_COMMON_H_
+
+#if __cplusplus
+extern "C" {
+#endif
+
+#define __STDC_CONSTANT_MACROS
+
+#include <libavdevice/avdevice.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavfilter/avfiltergraph.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/frame.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
+/* https://ffmpeg.org/doxygen/2.0/group__lavu__misc.html#ga9a84bba4713dfced21a1a56163be1f48 for AVMediaType*/
+#include <libavutil/avutil.h>
+
+#if __cplusplus
+};
+#endif
+#include "log.hpp"
+
+
+
+#endif
